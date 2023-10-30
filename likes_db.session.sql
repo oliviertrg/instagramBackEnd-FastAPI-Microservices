37f042092f7d866fad7431cf43ea87198f6aa92b
oliviertrg <rioverrain@gmail.com>
2023-10-30 14:48:23 -0400

    update\ for likes-services

--- likes_db.session.sql
+++ likes_db.session.sql
@@ -5,4 +5,66 @@ CREATE TABLE likes (
     liked_at timestamp
 );
 
-SELECT * FROM likes ;
+SELECT * FROM likes  where post_id = '1234';
+
+SELECT count(*) FROM likes  where post_id = '123';
+
+
+INSERT INTO likes(post_id,user_id,liked_at) 
+                   VALUES (
+            '123',
+            '1',
+            '2023-10-30T17:03:33.088500') 
+                   WHERE NOT EXISTS (post_id,user_id,liked_atpost_id,user_id,liked_at);
+
+
+INSERT INTO likes
+    (post_id,user_id,liked_at)
+SELECT '1234',
+            '1',
+            '2023-10-30T17:03:33.088500'
+WHERE
+    NOT EXISTS (
+        SELECT user_id FROM likes 
+                   WHERE post_id = '1234'
+    );
+
+
+
+SELECT count(user_id) FROM likes WHERE post_id = '123';
+
+SELECT user_id FROM likes WHERE 
+                   user_id NOT IN (SELECT user_id FROM likes 
+                   WHERE post_id = '123');
+
+
+
+
+
+
+INSERT INTO customers (name, email)
+VALUES ('John Doe', 'john.doe@example.com')
+WHERE NOT EXISTS (SELECT email FROM customers 
+WHERE email = 'john.doe@example.com');
+
+
+
+
+
+
+
+CREATE TABLE customers (
+    name text,
+    email text
+);
+
+SELECT * FROM customers ;
+
+INSERT INTO customers
+    (name, email)
+SELECT 'John Doe', 'john.doe@example.com'
+WHERE
+    NOT EXISTS (
+        SELECT email FROM customers 
+WHERE email = 'john.doe@example.com'
+    );
--- likes_services/app/likes.py
+++ likes_services/app/likes.py
@@ -29,33 +29,24 @@ async def view(post_id:str,current_users : int = Depends(auth2.get_current_user)
         db = curso()
         c = db.cursor()
  
-        sql = (f'''SELECT * FROM "likes" where "post_id" = '{post_id}' ;''')
+        sql = (f"""SELECT * FROM "likes" where "post_id" = '{post_id}' ;""")
         c.execute(sql)
-        l = c.fetchall()
-        db.close()
-        # x = (
-        #     schema.likes(
-        #     post_id=i[0],
-        #     user_id=i[1],
-        #     created_at=str(i[2]),
+        x = (
+            schema.likes(
+            post_id=i[1],
+            user_id=i[2],
+            created_at=str(i[3]),
             
-        # ).dict()
-        # for (i) in session.execute(f"""SELECT * FROM post_likes.likes  WHERE post_id  = '{post_id}';""") 
-        #     )
+        ).dict()
+        for i in c.fetchall()
+            )
+        db.close()
     except Exception as e:
          print(f"Error {e}")
          raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                                  detail="Not authorized to perform requested action")
-         # x = (
-        #     schema.likes(
-        #     post_id=i[0],
-        #     user_id=i[1],
-        #     created_at=str(i[2]),
-            
-        # ).dict()
-        # for (i) in session.execute(f"""SELECT * FROM post_likes.likes  WHERE post_id  = '{post_id}';""") 
-        #     )
-    return {"data":l} 
+
+    return {"data":x} 
 
 @router.post("/{post_id}/like/")
 async def test(post_id: str,current_users : int = Depends(auth2.get_current_user)):
@@ -66,43 +57,44 @@ async def test(post_id: str,current_users : int = Depends(auth2.get_current_user
         post_id = post_id     
         ).dict()   
     
-        session = csd() 
-        # x = session.execute(f"""SELECT count(user_id) FROM post_likes.likes  
-        #                     WHERE post_id = '{post_id}' and user_id = '{current_users.id}' ; """) ;
-        # print(x)
-        # print(x[0][0])
-        # if  x[0][0] == 0 :
-        x = session.execute(f'''INSERT INTO post_likes.likes (post_id,user_id,liked_at)
-                        VALUES ('{post_id }','{current_users.id}','{datetime.now()}') ;
-                                 WHERE user_id NOT IN (
-                                                      SELECT user_id FROM post_likes.likes
-                                                            WHERE post_id = '{post_id}' 
-                                                            )    ;
-                                ''')
-        # else :
-        #      return Response(status_code=status.HTTP_403_FORBIDDEN,
-        #                             content={"detail":"Not authorized to perform requested action "})
+        db = curso()
+        c = db.cursor()
+        sql = (f"""INSERT INTO likes
+                                (post_id,user_id,liked_at)
+                            SELECT '{post_id}','{current_users.id}','{datetime.now()}'
+                            WHERE NOT EXISTS 
+                                (
+                                    SELECT user_id FROM likes 
+                                            WHERE post_id = '{post_id}'
+                                ); """ )
+        c.execute(sql)
+        db.commit()
+        db.close()
+
     except Exception as e:
          print(f"Error {e}")
          raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                                  detail="Not authorized to perform requested action")
     return x
 
-@router.delete("/{comment_id}/detele/")
-async def test(comment_id: str,current_users : int = Depends(auth2.get_current_user)):
+@router.delete("/{post_id}/detele/")
+async def test(post_id: str,current_users : int = Depends(auth2.get_current_user)):
    try : 
 
-    session = csd() 
-    x = tuple(session.execute(f'''SELECT * FROM comment.photo_comments
-                        WHERE id = {comment_id} ; '''))
+    db = curso()
+    c = db.cursor()
+    c.execute(f"""SELECT user_id FROM likes
+                        WHERE post_id = '{post_id}' and user_id = '{current_users.id}'; """)
+    x = c.fetchall()
     if len(x) != 0:
            
-        if int(current_users.id) == int(x[0][1]) :
+        if int(current_users.id) == int(x[0][0]) :
 
-            session.execute(f'''DELETE FROM comment.photo_comments
-                            WHERE id = {comment_id} ; ''')
+            c.execute(f'''DELETE FROM likes
+                            WHERE post_id = '{post_id}' and user_id = '{current_users.id}' ; ''')
+            db.commit()
+            db.close()
         else :
-
             return Response(status_code=status.HTTP_403_FORBIDDEN,
                                     content={"detail":"Not authorized to perform requested action "})
     else:
